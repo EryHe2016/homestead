c10fb8c63617f576fa1e55863f7b18682d2c56be
ery.he <ery.he@feisu.com>
2021-03-23 09:55:16 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,23 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-  <title>@yield('title','Weibo App')</title>
-</head>
-<body>
-@yield('content')
-</body>
-</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
 
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">Weibo App</a>
+                <ul class="navbar-nav justify-content-end">
+                    <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                    <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
