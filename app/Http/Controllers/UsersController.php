d07132006be4ca4dffecfa1f0cad878b9fed45c4
ery.he <ery.he@feisu.com>
2021-03-24 06:08:42 +0000

    表单验证中文的错误提示信息添加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,32 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    /**
+     * 展示用户注册表单页面
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * 展示当前客户信息页面
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return ;
     }
 }
