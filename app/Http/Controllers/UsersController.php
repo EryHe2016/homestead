8c9d7a7687360b63014637b659cd5615a85604f8
ery.he <ery.he@feisu.com>
2021-06-21 09:39:48 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -104,4 +104,12 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '删除用户成功');
+        return back();
+    }
 }
