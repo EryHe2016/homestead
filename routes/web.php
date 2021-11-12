732601bd14785f6375c245cbe1da06b85c2b11cc
ery.he <ery.he@feisu.com>
2021-11-12 06:48:48 +0000

    删除微博

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,4 @@ Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('
 Route::post('/password/reset', 'PasswordController@reset')->name('password.update');
 
 //微博路由
-Route::resource('statuses', 'StatusesController', ['only' => ['create', 'store']]);
+Route::resource('statuses', 'StatusesController', ['only' => ['create', 'store', 'destroy']]);
